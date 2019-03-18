3e916c537b32ac5191af3a75636f8d6dcd4c76cc
delanobgt <irvindelano@gmail.com>
2019-03-18 15:58:22 +0700

    almost done again

--- src/components/main/Footer.js
+++ src/components/main/Footer.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import { compose } from "redux";
 import { withStyles } from "@material-ui/core/styles";
-import Typography from "@material-ui/core/Typography";
-import { Grid, Divider, Hidden } from "@material-ui/core";
+// import Typography from "@material-ui/core/Typography";
+// import { Grid, Divider, Hidden } from "@material-ui/core";
 
 const styles = theme => ({
   root: {
@@ -33,70 +33,83 @@ class Footer extends Component {
     const { classes, passedRef } = this.props;
 
     return (
-      <div ref={passedRef}>
-        <Grid
-          container
-          justify="center"
-          alignItems="center"
-          className={classes.root}
-        >
-          <Grid item xs={10} sm={1} md={1} lg={1}>
-            <Typography variant="body2" className={classes.title}>
-              Organized by:
-            </Typography>
-            <div>
-              <img
-                alt=""
-                src={require("../../res/images/logos/bem_logo.png")}
-                className={classes.logo}
-              />
-            </div>
-          </Grid>
-          <Hidden xsDown>
-            <Grid
-              item
-              sm={1}
-              md={1}
-              lg={1}
-              style={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center"
-              }}
-            >
-              <Divider className={classes.divider} />
-            </Grid>
-          </Hidden>
-          <Hidden smUp>
-            <Grid item xs={10} sm={10} md={10} lg={10}>
-              <Divider style={{ margin: "0.65em 0" }} />
-            </Grid>
-          </Hidden>
-          <Grid item xs={10} sm={6} md={7} lg={6}>
-            <Typography variant="body2" className={classes.title}>
-              Supported by:
-            </Typography>
-            <div>
-              {[
-                "hmfik_logo.png",
-                "hmpsm_logo.png",
-                "hmpsa_logo.png",
-                "hmfh_logo.png",
-                "sgs_logo.png",
-                "sl_logo.png",
-                "uph_logo.jpeg"
-              ].map(logoName => (
-                <img
-                  alt=""
-                  src={require(`../../res/images/logos/${logoName}`)}
-                  className={classes.logo}
-                />
-              ))}
-            </div>
-          </Grid>
-        </Grid>
+      <div
+        ref={passedRef}
+        style={{ textAlign: "center", backgroundColor: "white" }}
+      >
+        <img
+          alt=""
+          src={require("../../res/images/footer.png")}
+          style={{ width: "100%", maxWidth: "800px" }}
+        />
       </div>
     );
+
+    // return (
+    //   <div ref={passedRef}>
+    //     <Grid
+    //       container
+    //       justify="center"
+    //       alignItems="center"
+    //       className={classes.root}
+    //     >
+    //       <Grid item xs={10} sm={1} md={1} lg={1}>
+    //         <Typography variant="body2" className={classes.title}>
+    //           Organized by:
+    //         </Typography>
+    //         <div>
+    //           <img
+    //             alt=""
+    //             src={require("../../res/images/logos/bem_logo.png")}
+    //             className={classes.logo}
+    //           />
+    //         </div>
+    //       </Grid>
+    //       <Hidden xsDown>
+    //         <Grid
+    //           item
+    //           sm={1}
+    //           md={1}
+    //           lg={1}
+    //           style={{
+    //             display: "flex",
+    //             alignItems: "center",
+    //             justifyContent: "center"
+    //           }}
+    //         >
+    //           <Divider className={classes.divider} />
+    //         </Grid>
+    //       </Hidden>
+    //       <Hidden smUp>
+    //         <Grid item xs={10} sm={10} md={10} lg={10}>
+    //           <Divider style={{ margin: "0.65em 0" }} />
+    //         </Grid>
+    //       </Hidden>
+    //       <Grid item xs={10} sm={6} md={7} lg={6}>
+    //         <Typography variant="body2" className={classes.title}>
+    //           Supported by:
+    //         </Typography>
+    //         <div>
+    //           {[
+    //             "hmfik_logo.png",
+    //             "hmpsm_logo.png",
+    //             "hmpsa_logo.png",
+    //             "hmfh_logo.png",
+    //             "sgs_logo.png",
+    //             "sl_logo.png",
+    //             "uph_logo.jpeg"
+    //           ].map(logoName => (
+    //             <img
+    //               alt=""
+    //               src={require(`../../res/images/logos/${logoName}`)}
+    //               className={classes.logo}
+    //             />
+    //           ))}
+    //         </div>
+    //       </Grid>
+    //     </Grid>
+    //   </div>
+    // );
   }
 }
 
